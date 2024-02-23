98f31e4714716e5f744ce215a3294040c60fd42f
naleeha <nlha86@gmail.com>
2024-02-23 17:14:42 +0000

    #1184 spiking httpclient using sttp

--- example/rest-api/src/test/scala/org/finos/vuu/example/rest/client/SttpHttpClientTest.scala
+++ example/rest-api/src/test/scala/org/finos/vuu/example/rest/client/SttpHttpClientTest.scala
@@ -3,21 +3,26 @@ package org.finos.vuu.example.rest.client
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
 import sttp.client4.UriContext
-import sttp.model.Uri
+import sttp.model.{StatusCode, Uri}
 
 class SttpHttpClientTest extends AnyFeatureSpec with Matchers {
 
+  //https://docs.scala-lang.org/toolkit/http-client-request.html#the-request-definition
   //https://sttp.softwaremill.com/en/latest/testing.html
   //https://sttp.softwaremill.com/en/latest/backends/akka.html
-  Scenario("simple get endpoint") {
+  Feature("Http client using sttp"){
 
-    val url: Uri = uri"http://google.com"
+    ignore("simple get endpoint") {
 
-    val httpClient = new SttpHttpClient()
-    val response = httpClient.get(url)
+      val url: Uri = uri"http://google.com" //todo replace with fake url we want to test with
 
-    response.statusText shouldEqual 200
-    response.body shouldEqual "some dummy body"
+      val httpClient = new SttpHttpClient()
+      val response = httpClient.get(url)
+
+      response.code shouldEqual StatusCode.Ok
+      response.body shouldEqual "some dummy body"
+    }
   }
+
 
 }
